1bb4443da01fcb11bb39c67ce796863bab4a4222
tphanson <tphanson@kambria.io>
2019-04-15 21:27:16 +0700

    update version

--- wallet/skin/react/core/footer.js
+++ wallet/skin/react/core/footer.js
@@ -22,7 +22,7 @@ class Footer extends Component {
   render() {
     return (
       <div className={cx("row", "wallet-footer")}>
-        <div className={cx("col-12", "col-sm-6", "col-md-4", "col-lg-3 mt-5")} style={{ "height": "50px" }}>
+        <div className={cx("col-12", "col-sm-6", "col-md-4", "col-lg-3", "mt-5")} style={{ "height": "50px" }}>
           <div className={cx("row")}>
             <div className={cx("col", "text-center")}>
               <button className={cx("circle-btn")}><i className={cx("prevent-fish")} /></button>
