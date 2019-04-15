df115e53d278c033757b15b74f6a8a7283060820
tphanson <tphanson@kambria.io>
2019-04-15 22:08:45 +0700

    modulize typography

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
 import store, { history } from 'store';
 
+import './index.css';
 import App from 'containers/App';
 
 render(
